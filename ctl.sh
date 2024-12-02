a69a64b9f14a7663fecbb749c7080c4f3bd115e5
Dan Stewart <git@mail.danstewart.dev>
2024-12-02 21:06:11 +0000

    Update deploy.sh

--- ctl.sh
+++ ctl.sh
@@ -10,7 +10,7 @@ while [[ "$#" -gt 0 ]]; do
 	case "$1" in
 		--serve) serve=1 ;;
 		--build) build=1 ;;
-        --deploy) build=1; deploy=1 ;;
+		--deploy) build=1; deploy=1 ;;
 		-h|--help) help=1 ;;
 		--) shift; break ;;
 	esac
@@ -48,5 +48,5 @@ if [[ $build == 1 ]]; then
 fi
 
 if [[ $deploy == 1 ]]; then
-    scp -r src/* nginx:/data/www/onion.danstewart.xyz/
+    scp -r src/* web:/var/www/onion.danstewart.xyz
 fi
